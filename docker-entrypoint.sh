e2bd7fe2c3707626f0c56d476d8a0ba562147a8e
sec77 <31564517+secure-77@users.noreply.github.com>
2024-12-14 19:49:18 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -65,7 +65,7 @@ fi
 # Mount and verify that something is present. davfs2 always creates a lost+found
 # sub-directory, so we can use the presence of some file/dir as a marker to
 # detect that mounting was a success. Execute the command on success.
-mount -t davfs $WEBDRIVE_URL $DEST -o uid=$OWNER,gid=$GROUP,dir_mode=755,file_mode=755
+mount -t davfs $WEBDRIVE_URL $DEST -o uid=$OWNER,gid=$GROUP,dir_mode=770,file_mode=770
 if [ -n "$(ls -1A $DEST)" ]; then
     echo "Mounted $WEBDRIVE_URL onto $DEST"
     exec "$@"
